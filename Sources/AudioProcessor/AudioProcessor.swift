4de31894a7dcc75f8c75d6afd193eab34cea090e
Sam Meech-Ward <sam@meech-ward.com>
2017-11-25 16:25:28 -0800

    Setup the audio processor to throw an error when not given the data it needs

--- Sources/AudioProcessor/AudioProcessor.swift
+++ Sources/AudioProcessor/AudioProcessor.swift
@@ -1,1 +1,22 @@
 
+enum AudioProcessorError: Error {
+    case noData
+    case noAmplitudeData
+}
+
+struct AudioProcessor {
+    let samples: [AudioSample]
+    
+    init(samples: [AudioSample]) {
+        self.samples = samples
+    }
+    
+    func processBasedOnAmplitude() throws {
+        for sample in samples {
+            guard let amplitude = sample.amplitude else {
+                throw AudioProcessorError.noAmplitudeData
+            }
+        }
+        throw AudioProcessorError.noData
+    }
+}
