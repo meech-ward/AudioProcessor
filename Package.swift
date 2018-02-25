da4e5644efdca3f995a2fce74a0ded2abcbb746f
Sam Meech-Ward <sam@meech-ward.com>
2018-02-24 16:06:32 -0800

    Remove all files that don't just process

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["AudioProcessor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ObserveSocial/Observe", from: "0.4.0"),
-        .package(url: "https://github.com/ObserveSocial/Focus", from: "0.5.0")
+        .package(url: "https://github.com/meech-ward/Observe", from: "0.5.1"),
+        .package(url: "https://github.com/meech-ward/Focus", from: "0.6.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
